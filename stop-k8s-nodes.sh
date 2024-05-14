0fa3b8929c13e1a908901b3a8dcc96bfc32599a7
root <root@nuc13>
2024-05-14 11:13:48 +0800

    把memory增加到10G，ssd增加60G，新增stop虚拟机的脚本。

--- stop-k8s-nodes.sh
+++ stop-k8s-nodes.sh
@@ -7,23 +7,22 @@ source ./config_file
 echo "The value of num_vm is: $num_vm"
 echo "The value of template_id is: $template_id"
 
-# 开始resize虚拟机
+# 开始stop虚拟机
 for ((i=0; i<$num_vm; i++)); do
     vm_name="k8s-$i"
     vm_id=$((6600 + i))  # 假设虚拟机ID从101开始递增
     hex_i=$(printf '%02X' $i)  # 将数字i转换为两位的十六进制数
     mac_address="AA:BB:CC:DD:EE:$hex_i"  # MAC地址
 
-    # resize虚拟机
-    echo "正在resize虚拟机 $vm_name MAC地址 $mac_address..."
-    qm resize $vm_id scsi0 +64G         # K8S 对 SSD 存储: 至少 40 GB
-    qm set $vm_id -memory 10240          # K8S 对 memory：worker节点至少8G
+    # 克隆虚拟机
+    echo "正在stop虚拟机 $vm_name MAC地址 $mac_address..."
+    qm stop $vm_id
 
-    # 检查启动操作是否成功
+    # 检查stop操作是否成功
     if [ $? -eq 0 ]; then
-        echo -e "\033[32mresize虚拟机 $vm_name 成功\033[0m"
+        echo -e "\033[32mstop虚拟机 $vm_name 成功\033[0m"
     else
-        echo -e "\033[31mresize虚拟机 $vm_name 失败\033[0m"
+        echo -e "\033[31mstop虚拟机 $vm_name 失败\033[0m"
     fi
 done
 
